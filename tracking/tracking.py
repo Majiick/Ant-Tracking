f84e0f9e49d6be85724ad4337754e543f62987b3
Zan Smirnov <c15437072@mydit.ie>
2018-09-24 19:34:05 +0100

    Added background subtraction

--- tracking/tracking.py
+++ tracking/tracking.py
@@ -1,34 +1,31 @@
 import numpy as np
 import cv2
 
-cap = cv2.VideoCapture('../video1.mp4')
-fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()
+cap = cv2.VideoCapture('../video1.mp4')  # Load capture video
+fgbg = cv2.bgsegm.createBackgroundSubtractorMOG()  # Create a background subtractor.
 
-while(cap.isOpened()):
-    ret, frame = cap.read()
-    fgmask = fgbg.apply(frame)
-    original_frame = frame.copy()
+while(cap.isOpened()):  # While video is opened
+    ret, frame = cap.read()  # Ret is just status, frame is the actual image.
+    fgmask = fgbg.apply(frame)  # Apply the frame to the background subtractor. fgmask is the result of subtracting the background.
+    original_frame = frame.copy()  # Save the original frame so we can use it for reference later on.
 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    frame = cv2.GaussianBlur(frame,(5,5),0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
-    h, s, v = cv2.split(hsv)
-    h.fill(255)
-    #for i in range(len(h.flat)):
-        #if h.flat[i] != 255:
-            #h.flat[i] = 0
-    s.fill(255)
-    # v.fill(255)
-    ret,v = cv2.threshold(v,127,255,cv2.THRESH_BINARY)
-    frame = cv2.merge([h, s, v])
-    frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
+    frame = cv2.GaussianBlur(frame,(5,5),0)  # Blur the image using a gaussian blur with a 5x5 kernel.
+    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)  # Convert image from RGB to HSV.
+    h, s, v = cv2.split(hsv)  # Get the H, S, and V channels.
+    h.fill(255)  # Set all hue values to 255
+    s.fill(255)  # Set all saturation values to 255
+    # Setting saturation and hue to 255 means that we only see the value.
+    # v.fill(255)  
+    ret,v = cv2.threshold(v,127,255,cv2.THRESH_BINARY)  # For any pixel with value of more than 127, turn that value into 255. Any pixel with value less than 255 set to 0.
+    frame = cv2.merge([h, s, v])  # Merge HSV into one image.
+    frame = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)  # Convert HSV to BGR
 
     # grey = cv2.cvtColor(frame, cv2.COLOR_HSV2BGR)
-    edges = cv2.Canny(frame,100,200)
-    cv2.imshow('frame', frame)
-    cv2.imshow('edges', edges)
-    cv2.imshow('original', original_frame)
-    cv2.imshow('fgmask',fgmask)
+    edges = cv2.Canny(frame,100,200)  # Use canny edge detection
+    cv2.imshow('frame', frame)  # Draw the frame image
+    cv2.imshow('edges', edges)  # Draw the canny edges
+    cv2.imshow('original', original_frame)  # Draw the original frame image
+    cv2.imshow('fgmask', fgmask)  # Draw the foreground mask
     if cv2.waitKey(20) & 0xFF == ord('q'):
         break
 
